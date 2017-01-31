eb2cb95f2def1daed6ec170e964a8e5382d704dd
VaJoy Larn <vajoy@qq.com>
2017-01-31 20:46:51 +0800

    modify

---
+++ gulp.dest.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var through2 = require('through2');
+var sourcemaps = require('gulp-sourcemaps');
+var duplexify = require('duplexify');
+var prepareWrite = require('../prepareWrite');
+var writeContents = require('./writeContents');
+
+function dest(outFolder, opt) {
+    if (!opt) {
+        opt = {};
+    }
+
+    // _transform 接口
+    function saveFile(file, enc, cb) {
+        // 写入文件之前的准备处理，主要是 opt 初始化、file对象的 path/base/cwd 等属性
+        // 修改为相对 outFolder 的路径，方便后面 writeContents 生成正确的目的文件
+        prepareWrite(outFolder, file, opt, function(err, writePath) {
+            if (err) {
+                return cb(err);
+            }
+            //通过 fs.writeFile / fs.createWriteStream 等接口来写入和创建目标文件/文件夹
+            writeContents(writePath, file, cb);
+        });
+    }
+
+    // 生成 sourcemap 文件（注意这里的 opt.sourcemaps 若有则应为指定路径）
+    var mapStream = sourcemaps.write(opt.sourcemaps);
+    var saveStream = through2.obj(saveFile);
+    // 合并为单条 duplex stream
+    var outputStream = duplexify.obj(mapStream, saveStream);
+
+    //生成目标文件/文件夹
+    mapStream.pipe(saveStream);
+
+    //依旧返回输出流（duplex stream），不过常规也不会有下游消费了
+    return outputStream;
+}
+
+module.exports = dest;
--- gulp.src.js
+++ gulp.src.js
@@ -56,7 +56,8 @@ function src(glob, opt) {
     // read 选项默认为 true，表示允许文件内容可读（为 false 时不可读 且将无法通过 .dest 方法写入硬盘）
     if (options.read !== false) {
         outputStream = outputStream
-            //获取文件内容，预设为 Buffer 时通过 fs.readFile 接口获取
+            //获取文件内容，写入file.contents 属性去。
+            //预设为 Buffer 时通过 fs.readFile 接口获取
             //否则为 Stream 类型，通过 fs.createReadStream 接口获取
             .pipe(getContents(options));
     }
@@ -67,7 +68,8 @@ function src(glob, opt) {
         outputStream = duplexify.obj(inputPass, merge(outputStream, inputPass));
     }
 
-    //是否要开启 sourcemap（默认为false）
+    //是否要开启 sourcemap（默认为false），若为 true 则将流推送给 gulp-sourcemaps 去初始化，
+    //后续在 dest 接口里再调用 sourcemaps.write(opt.sourcemaps) 将 sourcemap 文件写入流
     if (options.sourcemaps === true) {
         outputStream = outputStream
             .pipe(sourcemaps.init({loadMaps: true}));
